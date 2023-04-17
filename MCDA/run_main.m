03407d74dde1bf3e8341e82445dbfd58cc985a53
ALittleLeek <87791500+kissbee@users.noreply.github.com>
2023-04-17 17:04:54 +0800

    Update run_main.m

--- MCDA/run_main.m
+++ MCDA/run_main.m
@@ -4,13 +4,9 @@ warning('off')
 addpath('data')
 addpath('func')
 
-dataname = 'MSRC';         % set dataset name here. 
+dataname = 'MSRC_v1';         % set dataset name here. 
 disp(['--test data:'  dataname ])
 
 load(dataname);            % load data
-
-for i = 1:size(X,2)
-    X{i} = X{i}';
-end
 
 Test_MCDA(X,Y);            % test dataset dimension: dv * n.
